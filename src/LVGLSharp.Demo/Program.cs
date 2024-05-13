eb21da93edde4eef6b91c003229479e05301612c
xcssa <1454643841@qq.com>
2024-05-13 18:17:34 +0800

    绑定修改为使用cppsharp生成

--- src/LVGLSharp.Demo/Program.cs
+++ src/LVGLSharp.Demo/Program.cs
@@ -1,37 +1,46 @@
-﻿using static LVGLSharp.Native;
-using static LVGLSharp.NativeWindows;
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using lvgl;
 
-lv_init();
+lv_init.LvInit();
 
-var display = lv_windows_create_display("Test", 800, 480, 100, false, false);
+var display = lv_windows_display.LvWindowsCreateDisplay("Test", 800, 480, 100, false, false);
 
-lv_windows_acquire_pointer_indev(display);
+lv_windows_input.LvWindowsAcquirePointerIndev(display);
+lv_windows_input.LvWindowsAcquireKeypadIndev(display);
+lv_windows_input.LvWindowsAcquireEncoderIndev(display);
 
-lv_windows_acquire_keypad_indev(display);
+var active = lv_display.LvDisplayGetScreenActive(lv_display.LvDisplayGetDefault());
 
-lv_windows_acquire_encoder_indev(display);
+lv_flex.LvObjSetFlexFlow(active, LvFlexFlowT.LV_FLEX_FLOW_COLUMN);
+lv_flex.LvObjSetFlexAlign(active, LvFlexAlignT.LV_FLEX_ALIGN_CENTER, LvFlexAlignT.LV_FLEX_ALIGN_START, LvFlexAlignT.LV_FLEX_ALIGN_CENTER);
 
-var active = lv_screen_active();
+var style = new LvStyleT();
+lv_style.LvStyleInit(style);
+var fontData = File.ReadAllBytes(@"C:\Windows\Fonts\msyh.ttc");
+var fontDataPtr = Marshal.AllocHGlobal(fontData.Length);
+Marshal.Copy(fontData, 0, fontDataPtr, fontData.Length);
+var font = lv_tiny_ttf.LvTinyTtfCreateData(fontDataPtr, (ulong)fontData.LongLength, 16);
+lv_style_gen.LvStyleSetTextFont(style, font);
 
-lv_obj_set_flex_flow(active, lv_flex_flow_t.LV_FLEX_FLOW_COLUMN);
-lv_obj_set_flex_align(active, lv_flex_align_t.LV_FLEX_ALIGN_CENTER, lv_flex_align_t.LV_FLEX_ALIGN_START, lv_flex_align_t.LV_FLEX_ALIGN_CENTER);
+var cb = lv_checkbox.LvCheckboxCreate(active);
+lv_obj_style.LvObjAddStyle(cb, style, 0);
+lv_checkbox.LvCheckboxSetText(cb, "测试");
 
-var cb = lv_checkbox_create(active);
-lv_checkbox_set_text(cb, "Apple");
-
-var button = lv_button_create(active);
-var label = lv_label_create(button);
+var button = lv_button.LvButtonCreate(active);
+var label = lv_label.LvLabelCreate(button);
 var i = 0;
-lv_obj_set_size(button, 100, 50);
-lv_obj_add_event_cb(button, e =>
+lv_obj_pos.LvObjSetSize(button, 100, 50);
+lv_obj_event.LvObjAddEventCb(button, e =>
 {
     i++;
-    lv_label_set_text(label, $"Click {i}");
-}, lv_event_code_t.LV_EVENT_CLICKED, IntPtr.Zero);
-lv_label_set_text(label, "Button");
-lv_obj_center(label);
+    lv_label.LvLabelSetText(label, $"点击 {i}");
+}, LvEventCodeT.LV_EVENT_CLICKED, 0);
+lv_obj_style.LvObjAddStyle(label, style, 0);
+lv_label.LvLabelSetText(label, "按钮");
+lv_obj_pos.LvObjAlign(label, (byte)LvAlignT.LV_ALIGN_CENTER, 0, 0);
 
 while (true)
 {
-    await Task.Delay(lv_timer_handler());
+    await Task.Delay((int)lv_timer.LvTimerHandler());
 }
